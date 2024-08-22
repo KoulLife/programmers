f3335894270418e7f9b83829c6b92b2b8eb28fde
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-08-22 12:45:11 +0900

    [level 2] Title: 더 맵게, Time: 1728.19 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,13 +2,13 @@ import heapq
 
 def solution(scoville, K):
     heapq.heapify(scoville)
-    res = 0
+    cnt = 0
     
     while scoville[0] < K:
         mix = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
         heapq.heappush(scoville, mix)
-        res += 1;
-        
-        if len(scoville) < 2 and scoville[0] < K:
+        cnt += 1
+        if scoville[0] < K and len(scoville) < 2:
             return -1
-    return res
+    
+    return cnt

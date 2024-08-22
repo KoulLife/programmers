3b1fe830e9b9d36ee195784e7530a531c12748be
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-08-22 13:04:56 +0900

    [level 2] Title: 타겟 넘버, Time: 372.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,16 @@
 def solution(numbers, target):
-    n = len(numbers)
-    answer = 0
-    def dfs(idx, result):
-        if idx == n:
-            if result == target:
-                nonlocal answer
-                answer += 1
+    # 1. i가 len(numbers)면 비교후 종료
+    # 2. v +와 - 비교
+    cnt = 0
+    
+    def dfs(i, v):
+        if i == len(numbers):
+            if v == target:
+                nonlocal cnt
+                cnt += 1
             return
         else:
-            dfs(idx+1, result+numbers[idx])
-            dfs(idx+1, result-numbers[idx])
+            dfs(i + 1, v + numbers[i])
+            dfs(i + 1, v - numbers[i])
     dfs(0,0)
-    return answer
+    return cnt

b7c16d78d3326482c86d76f4bcc4affd69b16bea
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2024-10-14 02:50:27 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        
+        boolean isFirst = true;
+        
+        int res = 0;
+        
+        for(int i = 0; i < included.length; i++){
+            if(included[i]){
+                res += (a + i * d);
+            }
+        }
+        return res;
+    }
+}

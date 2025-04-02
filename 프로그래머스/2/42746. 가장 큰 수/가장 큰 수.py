cfea5baa733dcc6a795ab9cc76b998b1095dce90
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-04-02 23:35:31 +0900

    [level 2] Title: 가장 큰 수, Time: 1412.44 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,7 @@
-def custom_key(x):
-    return x * 3
-
 def solution(numbers):
-    num = list(map(str, numbers))
-    num.sort(key=custom_key, reverse=True)
-    
-    res = str(''.join(num))
-    return '0' if res[0] == '0' else res
+    ans = ""
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x*3, reverse = True)
+    for i in numbers:
+        ans += i
+    return str(int(ans))

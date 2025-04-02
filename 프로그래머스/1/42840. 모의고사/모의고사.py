978a72d922ed2344df8baf617f1041a6c9d62ea5
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-04-02 23:46:05 +0900

    [level 1] Title: 모의고사, Time: 4.40 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,23 @@
 def solution(answers):
-    pattern1 = [1,2,3,4,5]
-    pattern2 = [2,1,2,3,2,4,2,5]
-    pattern3 = [3,3,1,1,2,2,4,4,5,5]
-    score = [0,0,0]
-    result = []
+    ans = []
     
-    for idx, answer in enumerate(answers):
-    	if answer == pattern1[idx%len(pattern1)]:
-        	score[0] += 1
-    	if answer == pattern2[idx%len(pattern2)]:
-        	score[1] += 1
-    	if answer == pattern3[idx%len(pattern3)]:
-        	score[2] += 1   
-    for idx, s in enumerate(score):
-    	if s == max(score):
-        	result.append(idx+1)
-    return result
+    stu_1 = [1, 2, 3, 4, 5]
+    stu_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    stu_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    scores = [0] * 3
+    
+    for i in range(len(answers)):
+        if answers[i] == stu_1[i % len(stu_1)]:
+            scores[0] += 1
+        if answers[i] == stu_2[i % len(stu_2)]:
+            scores[1] += 1
+        if answers[i] == stu_3[i % len(stu_3)]:
+            scores[2] += 1
+    
+    max_score = max(scores[0], scores[1], scores[2])
+    
+    for i in range(len(scores)):
+        if scores[i] == max_score:
+            ans.append(i + 1)
+    return ans

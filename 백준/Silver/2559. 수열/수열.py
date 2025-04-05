7675264ae426885b6de79069cff8057da621e667
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-04-06 02:37:08 +0900

    [Silver III] Title: 수열, Time: 96 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,20 +1,14 @@
-import sys
-input = sys.stdin.readline
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+res = -1e9
 
-N, K = map(int, input().split())
-T = list(map(int, input().split()))
-pSum = [0] * N
-pSum[0] = T[0]
+# 누적합 만들기
+pSum = [0] * (N+1)
+pSum[0] = 0
+for i in range(1, N+1):
+    pSum[i] = pSum[i-1] + A[i-1]
+# 시작 지점 확인
+for i in range(M, N+1):
+    res = max(res, pSum[i] - pSum[i - M])
 
-# prefix sum
-for i in range(1, N):
-  pSum[i] = pSum[i - 1] + T[i]
-
-res = []
-for j in range(0, N - K + 1):
-  if j == 0:
-    sum = pSum[j + K - 1]
-  else:
-    sum = pSum[j + K -1] - pSum[j - 1]
-  res.append(sum)
-print(max(res))
+print(res)

d9217500b9a704f4e1b46b7b1d4c391455fce015
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-04-08 20:32:31 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,22 @@
 def solution(genres, plays):
-    D1 = {}
-    D2 = {}
+    #{g : p}
+    dic1 = {}
+    #{g : (1,p)}
+    dic2 = {}
+    
     ans = []
     
     for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in D1:
-            D1[g] = p
+        if g not in dic1:
+            dic1[g] = p
         else:
-            D1[g] += p
-        if g not in D2:
-            D2[g] = [(i, p)]
+            dic1[g] += p
+        if g not in dic2:
+            dic2[g] = [(i,p)]
         else:
-            D2[g].append((i, p))
+            dic2[g].append((i,p))
     
-    # sort & [:2]
-    # genre : D1
-    for (D1_g, D1_p) in sorted(D1.items(), key = lambda x:x[1], reverse = True):
-        for (D2_i, D2_p) in sorted(D2[D1_g], key = lambda x:x[1], reverse = True)[:2]:
-            ans.append(D2_i)
-    
+    for d1_g, d1_p in sorted(dic1.items(), key = lambda x:x[1], reverse = True):
+        for d2_i, d2_p in sorted(dic2[d1_g], key = lambda x:x[1], reverse = True)[:2]:
+            ans.append(d2_i)
     return ans
-    

6b86bd83324100704aacc6ff4b8c085076982e62
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-04-21 12:38:26 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -0,0 +1,13 @@
+B = input().split('.')  # . 기준으로 나눔
+ans = []
+
+for part in B:
+    length = len(part)
+    if length % 2 != 0:
+        print(-1)
+        exit()
+    a_count = length // 4
+    b_count = (length % 4) // 2
+    ans.append('AAAA' * a_count + 'BB' * b_count)
+
+print('.'.join(ans))

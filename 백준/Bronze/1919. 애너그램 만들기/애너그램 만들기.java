c27dc86b8520a31ae35ce67c4218c44d1fcb6b61
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-08-18 08:06:48 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 172 ms, Memory: 17592 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+public class Main{
 
-    String str1 = sc.next();
-    String str2 = sc.next();
-    String res = "";
-
-    for (int i = 0; i < str2.length(); i++) {
-      char[] ch = new char[1];
-      ch[0] = str2.charAt(i);
-
-      if (str1.contains(new String(ch))) {
-        int idx = str1.indexOf(ch[0]);
-        str1 = str1.substring(0, idx) + str1.substring(idx + 1, str1.length());
-      } else {
-        res += new String(ch);
-      }
+    static int[] calAlphabet(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'a']++;
+        }
+        return count;
     }
-    System.out.println(res.length() + str1.length());
-  }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        // cal Alphabet count
+        int[] countStr1 = calAlphabet(str1);
+        int[] countStr2 = calAlphabet(str2);
+
+        // get Num
+        int res = 0;
+        for (int i = 0; i < 26; i++) {
+            res += Math.abs(countStr1[i] - countStr2[i]);
+        }
+        System.out.println(res);
+    }
 }

4c7f064948f6ba538738e2557d5c671f2898e6fe
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-08-19 01:20:46 +0900

    [Silver III] Title: 두 수의 합, Time: 724 ms, Memory: 95820 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,38 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lenOfArr = sc.nextInt();
+		int[] arr = new int[lenOfArr];
+		for(int i = 0; i < lenOfArr; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int goalVal = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
+		boolean[] checkVal = new boolean[1000001];
 
-        int res = 0;
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+		for (int num : arr) {
+			checkVal[num] = true;
+		}
 
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+		int res = 0;
+		for (int num : arr) {
+			int pairNum = goalVal - num;
+			if (0 <= pairNum && pairNum <= 1000000 && checkVal[pairNum]) {
+				res += 1;
+			}
+		}
 
-        Arrays.sort(arr);
-
-        int goal = sc.nextInt();
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while(left < right) {
-            if (arr[left] + arr[right] < goal) {
-                left += 1;
-            } else if (arr[left] + arr[right] > goal) {
-                right -= 1;
-            }
-
-            if (arr[left] + arr[right] == goal) {
-                res += 1;
-                left += 1;
-                right -= 1;
-            }
-        }
-        System.out.println(res);
-    }
+		System.out.println(res/2);
+	}
 }

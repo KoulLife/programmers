2212b49b2ee0f75b79e18dfb14791ab2520a1225
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-09-06 02:07:01 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,9 @@
 def solution(array, commands):
     res = []
-    # commands의 갯수만큼
-    for C in commands:
-        # array 복사한 c_array
-        c_array = array[:]
-        c_array = c_array[C[0]-1:C[1]]
-        c_array.sort()
-        res.append(c_array[C[2] - 1])
-    
+
+    for [i, j, k] in commands:
+        arr = array[(i-1):j]
+        arr.sort()
+        res.append(arr[k-1])
+
     return res
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.01 ms
+메모리: 9.31 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 29일 00:17:58
+2025년 09월 06일 02:06:58
 
 ### 문제 설명
 

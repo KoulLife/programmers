c4b0c7d2388bfb0b7c995ebabd477c9dd557e0cb
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-09-07 20:18:33 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,11 +1,12 @@
 def solution(n, lost, reserve):
-    reserve_set = set(reserve)-set(lost)
-    lost_set = set(lost)-set(reserve)
-    
-    for r in reserve_set:
-        if r-1 in lost_set:
-            lost_set.remove(r-1)
-        elif r+1 in lost_set:
-            lost_set.remove(r+1)
-            
-    return n-len(lost_set)
+    # 교집합 처리 (자기 자신은 빌려줄 수 없음)
+    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost)
+
+    for r in sorted(reserve_set):
+        if r - 1 in lost_set:
+            lost_set.remove(r - 1)
+        elif r + 1 in lost_set:
+            lost_set.remove(r + 1)
+
+    return n - len(lost_set)

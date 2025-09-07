9963d9252a7fc5cd7610fd41c22182b55465d6be
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-09-07 17:44:32 +0900

    [level 1] Title: 최소직사각형, Time: 5.99 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,13 +1,10 @@
 def solution(sizes):
-    # 1. big, small을 생성 0으로 초기화
-    # 2. for 루프로 sizes를 순회
-    # 3. 2개 중 큰 것은 큰것과 비교, 작은 것은 작은것과 비교
+    num_b = []
+    num_s = []
     
-    big, small = 0, 0
+    for [s1, s2] in sizes:
+        num_b.append(max(s1, s2))
+        num_s.append(min(s1, s2))
     
-    for i in sizes:
-        big = max(big, max(i))
-        small = max(small, min(i))
-    
-    answer = big * small
-    return answer
+    return (max(num_b) * max(num_s))
+        

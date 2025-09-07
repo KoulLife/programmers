b9fe69d5e757cadda7da48dc2ae7e6af508483c0
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-09-07 18:00:53 +0900

    [level 1] Title: 모의고사, Time: 3.40 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,32 @@
-def solution(answers):
-    ans = []
+def solution(answers):    
+    # 학생의 찍기 패턴 적용
+    pattern_s1 = [1, 2, 3, 4, 5]
+    pattern_s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern_s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    stu_1 = [1, 2, 3, 4, 5]
-    stu_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    stu_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    # 학색 점수 초기화    
+    c_s1 = 0
+    c_s2 = 0
+    c_s3 = 0
     
-    scores = [0] * 3
-    
+    # 점수 for문 확인, 
     for i in range(len(answers)):
-        if answers[i] == stu_1[i % len(stu_1)]:
-            scores[0] += 1
-        if answers[i] == stu_2[i % len(stu_2)]:
-            scores[1] += 1
-        if answers[i] == stu_3[i % len(stu_3)]:
-            scores[2] += 1
+        if answers[i] == pattern_s1[i%len(pattern_s1)]:
+            c_s1 += 1
+        if answers[i] == pattern_s2[i%len(pattern_s2)]:
+            c_s2 += 1
+        if answers[i] == pattern_s3[i%len(pattern_s3)]:
+            c_s3 += 1
     
-    max_score = max(scores[0], scores[1], scores[2])
+    score_list = [c_s1, c_s2, c_s3]
     
-    for i in range(len(scores)):
-        if scores[i] == max_score:
-            ans.append(i + 1)
-    return ans
+    max_score = 0
+    res = []
+    for [i, score] in enumerate(score_list):
+        if score > max_score:
+            max_score = score
+            res = [i + 1]
+        elif score == max_score:
+            res.append(i + 1)
+                
+    return res

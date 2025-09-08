e70f2908e658507d42ec30d12196e382f5d785a2
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-09-09 01:35:29 +0900

    [level 2] Title: 구명보트, Time: 7.76 ms, Memory: 9.62 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,14 @@
 def solution(people, limit):
-    people = sorted(people)
+    people.sort()
     l = 0
     r = len(people) - 1
     two_boat = 0
     
-    while l < r:
+    while(l < r):
         if people[l] + people[r] <= limit:
             l += 1
             two_boat += 1
         r -= 1
     
     return len(people) - two_boat
+            

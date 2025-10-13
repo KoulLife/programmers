b643860ad19eb2a66ff7a6fc65e78fb66af247f9
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2025-10-14 00:01:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.20 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,12 @@
 def solution(participant, completion):
-    P = {}
-    C = {}
-    res = ""
+    hashDict = {}
+    sumHash = 0
     
-    for c in completion:
-        if c not in C.keys():
-            C[c] = 1
-        else:
-            C[c] += 1
+    for part in participant:
+        hashDict[hash(part)] = part
+        sumHash += hash(part)
     
-    for p in participant:
-        if p not in P.keys():
-            P[p] = 1
-        else:
-            P[p] += 1
+    for comp in completion:
+        sumHash -= hash(comp)
     
-    for s in P.keys():
-        if s not in C.keys() or P[s] != C[s]:
-            res = s
-            break
-    return res
-    
+    return hashDict[sumHash]

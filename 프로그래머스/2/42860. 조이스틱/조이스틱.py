52ea3e52ffc1bd66b1f835a464740b891cd8efde
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-01-06 12:26:50 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,15 +1,19 @@
 def solution(name):
+    
     res = 0
     min_move = len(name) - 1
     
-    for i, c in enumerate(name):
-        res += min((ord(c) - ord('A')), (ord('Z') - ord(c) + 1))
+    for i, s in enumerate(name):
+        res += min((ord(s) - ord('A')), (ord('Z') - ord(s) + 1))
         
         next = i + 1
+
         while next < len(name) and name[next] == 'A':
             next += 1
+
+        min_move = min(min_move, 2 * i + len(name) - next, i + 2 * (len(name) - next))
+    
+    return res + min_move
+    
         
-        min_move = min(min_move, i * 2 + len(name) - next, i + 2 * (len(name) - next))
-    res += min_move
-    return res
         

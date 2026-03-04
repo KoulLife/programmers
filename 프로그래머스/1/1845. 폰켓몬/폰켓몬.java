9592ae43069f27bea24f5fe7234f8ad1817d5adb
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-03-05 01:03:07 +0900

    [level 1] Title: 폰켓몬, Time: 4.09 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        // M1. 배열 길이
-        int arrLength = nums.length / 2;
         
-        Map<Integer, Integer> hashMap = new HashMap<>();
+        int maxNum = nums.length / 2;
         
-        for (int num : nums){
-            hashMap.put(num, 0);
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {            
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        int answer = Math.min(arrLength, hashMap.size());
+        int res = Math.min(maxNum, map.size());
         
-        return answer;
+        return res;
     }
 }

4fbe1c78926906ceeecaefbe2efda87377f0f439
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-03-09 22:10:35 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 62.07 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    
+    time = 0
+    bridge = deque( [0] * bridge_length )
+    truck_weights = deque(truck_weights)
+    
+    curr_weight = 0
+    
+    while bridge:
+        time += 1
+        
+        exited_truck = bridge.popleft()
+        curr_weight -= exited_truck
+        
+        if truck_weights:
+            if curr_weight + truck_weights[0] <= weight:
+                new_truck = truck_weights.popleft()
+                bridge.append(new_truck)
+                curr_weight += new_truck
+            else:
+                bridge.append(0)
+    
+    return time

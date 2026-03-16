3a14463d8b1fa53e9188f649e9622562f83fb2d9
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-03-16 21:46:23 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,12 @@
 def solution(brown, yellow):
     res = []
 
-    for y in range(1, yellow + 1):
-        if yellow % y != 0:
+    for i in range(1, yellow + 1):        
+        if yellow % i != 0:
             continue
-
-        x = yellow // y
-        tmp = (x + 2) * 2 + (y + 2) * 2
-        if (tmp) == brown + 4:
-            res = [x + 2, y + 2]
-            break
-
+        else:
+            j = yellow // i
+            if ((j + 2) * 2) + (i * 2) == brown:
+                res = [j + 2, i + 2]
+                break
     return res

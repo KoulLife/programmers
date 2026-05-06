6f9a93845fc27437c71b7dc3dd4f83faad5f03fa
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-05-06 20:42:46 +0900

    [level 2] Title: 구명보트, Time: 9.88 ms, Memory: 9.67 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,14 +1,24 @@
 def solution(people, limit):
-    people.sort()
-    l = 0
-    r = len(people) - 1
-    two_boat = 0
+    # 최대 2명
+    # greedy 사용
+    # 50000 -> O(n^2) 가능
+    # 80, 70, 50, 50
     
-    while(l < r):
+    people.sort(reverse = True)
+    
+    l, r = 0, len(people)-1
+    res = 0
+    
+    while(l <= r):
+        if l == r:
+            res += 1
+            break
         if people[l] + people[r] <= limit:
             l += 1
-            two_boat += 1
-        r -= 1
+            r -= 1
+        else:
+            l += 1
+        res += 1
     
-    return len(people) - two_boat
-            
+    return res
+        

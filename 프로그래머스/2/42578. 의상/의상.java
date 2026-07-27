e293bb271c56587ee367ff8ec339c0bf4cae8e3c
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-07-27 20:17:49 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,24 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
+        
         HashMap<String, Integer> map = new HashMap<>();
         
-        for (String[] c : clothes) {
-            String type = c[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+        for(String[] c : clothes) {            
+            if (map.containsKey(c[1])) {
+                map.put(c[1], map.get(c[1]) + 1);
+            } else {                
+                map.put(c[1], 2);
+            }
         }
         
         int answer = 1;
         
-        for(int count : map.values()) {
-            answer *= (count + 1);
+        for(HashMap.Entry<String, Integer> val : map.entrySet()) {
+            answer *= val.getValue();
         }
+        
         
         return answer - 1;
     }

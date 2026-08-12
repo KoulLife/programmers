18e47d776176dbc804d41d987143efe69104e92a
Jang Dong Ik <111228362+KoulLife@users.noreply.github.com>
2026-08-12 21:12:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.18 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?gad_source=1&gad_campaignid=23776046558&gbraid=0AAAAAC_c4nBTkQYpuQ1ee-pLOAO46yhBQ&gclid=CjwKCAjws_DTBhB_EiwAXZknGS0LkqpuiUZo_hJudChN6Ey601Q2CjqD-D85Fl9d34TjpF6LmRMm1BoCjAMQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 15.49 ms
+메모리: 11.3 MB, 시간: 7.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 09일 20:36:50
+2026년 08월 12일 21:12:51
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,36 +1,29 @@
 from collections import deque
 
-def solution(maps):
-    answer = -1    
-    # 시작점 (0,0)에서 거리 1부터 시작 (문제 조건)
-    queue = deque([ ([0, 0], 1) ])
+def bfs(boards):
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    dq = deque()
+    dq.append((0,0,1))
+    boards[0][0] = 0
     
-    target_y = len(maps) - 1
-    target_x = len(maps[0]) - 1
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, 1, -1]
-    
-    # 시작 위치도 방문 처리 (중요!)
-    maps[0][0] = 0
-    
-    while queue:
-        curr_loc, dist = queue.popleft()
-        y, x = curr_loc[0], curr_loc[1]
-        
-        # 도착했을 때
-        if y == target_y and x == target_x:
-            return dist
-        
+    while dq:
+        before_y,before_x, cnt = dq.popleft()
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            
-            # 1. 맵 범위 안에 있는지 확인
-            if 0 <= ny <= target_y and 0 <= nx <= target_x:
-                # 2. 벽이 아니고(1) 아직 방문하지 않았는지 확인
-                if maps[ny][nx] == 1:
-                    maps[ny][nx] = 0  # 다시 방문하지 않도록 벽으로 만듦
-                    queue.append(([ny, nx], dist + 1))
+            y = before_y + dy[i]
+            x = before_x + dx[i]
+            if y < 0 or y >= len(boards) or x < 0 or x >= len(boards[0]) or boards[y][x] == 0:
+                continue
+            elif y == len(boards)-1 and x == len(boards[0])-1:
+                return cnt + 1
+            else:
+                boards[y][x] = 0
+                dq.append((y,x,cnt+1))
+    return -1
+
+def solution(maps):
+    boards = []
+    for m in maps:
+        boards.append(list(m))
     
-    return answer
+    return bfs(boards)
